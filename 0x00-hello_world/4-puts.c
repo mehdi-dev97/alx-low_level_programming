9d3adddc25f04bcc86e2650978e2fa1408610263
Mehdi Ait Mouh <mehdi.aitmouh.dev@gmail.com>
2023-02-16 07:53:08 -0800

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,17 @@
 #include <stdio.h>
 
+/**
+ * main - Print string in console using puts func.
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
-	char str[] = "Programming is like building a multilingual puzzle";
+	char str[] = "\"Programming is like building a multilingual puzzle";
 
 	puts(str);
 
-	return 0;
+	return (0);
 
 }
