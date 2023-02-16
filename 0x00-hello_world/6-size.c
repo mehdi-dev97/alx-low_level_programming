9d4412dca61e71a19dbf05f7caf1bd4d9e90a05c
Mehdi Ait Mouh <mehdi.aitmouh.dev@gmail.com>
2023-02-16 08:28:36 -0800

    Size is not grandeur, and territory does not make a nation

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,24 +2,22 @@
 
 /**
  * main - Entry point
- * Description Print Data type size in console using printf func.
  *
- * Return: Always 0 (Success)
- */
+ * Description: using sizeof to print sizes
+ *
+ * Return: Always 0 (Succesful)
+*/
 
 int main(void)
 {
 
-	printf("Integer size: %d byte(s)", sizeof(int));
+	/* sizeof evaluates the size of a variable */
+	printf("Size of a char: %d byte(s)\n", sizeof(char));
+	printf("Size of an int: %d byte(s)\n", sizeof(int));
+	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %d byte(s)\n", sizeof(float));
 
-	printf("Char size: %d byte(s)", sizeof(char));
-
-	printf("Float size: %d byte(s)", sizeof(float));
-	
-	printf("Long Integer size: %d byte(s)", sizeof(long int));
-	
-	printf("Long Long Integer: %d byte(s)", sizeof(long long int));
-	
 	return (0);
 
 }
