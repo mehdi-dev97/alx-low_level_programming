a1f80a7e34856b0b70a9bb7d171dea40fa16c719
Mehdi Ait Mouh <mehdi.aitmouh.dev@gmail.com>
2023-02-16 08:00:23 -0800

    Create c file to showclear

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Print string in console using printf func.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char str[] = "with proper grammar, but the outcome is a piece of art,";
+
+	printf("%s", str);
+
+	return (0);
+
+}
