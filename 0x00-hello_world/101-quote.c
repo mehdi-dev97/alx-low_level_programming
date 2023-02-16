082a965bdeb36a61d3a151d9df2109c5cc5f9982
Mehdi Ait Mouh <mehdi.aitmouh.dev@gmail.com>
2023-02-16 15:20:30 -0800

    Print string

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19"
+ * main - FPrintsf "and that piece of art is useful" - Dora Korpar, 2015-10-19"
  *
  * Return: Always 1.
  */
