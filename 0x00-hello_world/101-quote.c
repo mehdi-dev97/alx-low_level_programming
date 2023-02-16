a3c2b7aa439855c666c3694b44aa45a345110476
Mehdi Ait Mouh <mehdi.aitmouh.dev@gmail.com>
2023-02-16 15:04:54 -0800

    UNIX is basically a simple operating system

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Main start func
+ * Description : Print string in console using fprintf func.
+ *
+ * Return: Always 1 (Success)
+ */
+
+int main(void)
+{
+
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+
+	return (1);
+
+}
