21e06515352cb6006ec67919dbd11868ec04951a
Mehdi Ait Mouh <mehdi.aitmouh.dev@gmail.com>
2023-02-16 15:40:16 -0800

    fprintf string

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,11 +4,11 @@
 /**
  * main - Print string in console using fprintf func.
  *
- * Return: Always 0 (Success)
+ * Return: Always 1 (Success)
  */
+
 int main(void)
 {
 	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-	
 	return (1);
 }
