8c330388a0534a68725a5f53d7dfc8ff27cfb839
Mehdi Ait Mouh <67864826+mehdi-dev97@users.noreply.github.com>
2023-02-17 01:31:27 +0100

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,13 +2,12 @@
 
 /**
  * main - Print phrase "and that piece of art is useful"
- 	- Dora Korpar, 2015-10-19"
  *
  * Return: Always 1 (Success)
  */
 int main(void)
 {
 	fprintf(stderr, "and that piece of art is useful\" - "
-                "Dora Korpar, 2015-10-19\n");
+        "Dora Korpar, 2015-10-19\n");
 	return (1);
 }
