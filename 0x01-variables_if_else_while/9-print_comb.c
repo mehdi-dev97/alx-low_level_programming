47e249f1a12604a994ffe8d8918e0981961cb66d
Mehdi Ait Mouh <67864826+mehdi-dev97@users.noreply.github.com>
2023-02-18 00:04:57 +0100

    Update 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,22 +1,25 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
  * main - Entry point
- * Description: prints possible combinations of single-digit numbers.
+ * Description: prints numbers with comma and space.
  * Return: Always 0 (success)
  */
 int main(void)
 {
 	int n;
-	
-	for (n = '0'; n <= '9'; n++)
+
+	for (n = '0'; n <= '9' ; n++)
 	{
 		putchar(n);
+
 		if (n != '9')
 		{
 			putchar(',');
 			putchar(' ');
 		}
 	}
+
 	putchar('\n');
 	return (0);
 }
