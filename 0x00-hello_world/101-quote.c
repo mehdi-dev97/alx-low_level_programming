e90688a8ecf7d6db6350e08cc44b204681653513
Mehdi Ait Mouh <67864826+mehdi-dev97@users.noreply.github.com>
2023-02-17 01:28:53 +0100

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 
 /**
- * main - Print phrase "and that piece of art is useful" - Dora Korpar, 2015-10-19"
+ * main - Print phrase "and that piece of art is useful"
+ 	- Dora Korpar, 2015-10-19"
  *
  * Return: Always 1 (Success)
  */
 int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	fprintf(stderr, "and that piece of art is useful\" - "
+                "Dora Korpar, 2015-10-19\n");
 	return (1);
 }
