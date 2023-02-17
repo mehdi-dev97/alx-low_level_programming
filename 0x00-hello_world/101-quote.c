a837f05ff74dd1244e8910cbafb4945c054eec88
Mehdi Ait Mouh <mehdi.aitmouh.dev@gmail.com>
2023-02-16 16:17:41 -0800

    String phrase

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - Prints a quote by Dora Korpar.
+ * main - Print phrase "and that piece of art is useful" - Dora Korpar, 2015-10-19"
  *
- * Return: Always 1.
+ * Return Always 1.
  */
 int main(void)
 {
-    fprintf(stderr, "and that piece of art is useful\" - "
-                "Dora Korpar, 2015-10-19\n");
+    fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
     return (1);
 }
