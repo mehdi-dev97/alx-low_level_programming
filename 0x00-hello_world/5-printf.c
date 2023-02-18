4382836cbdcff236c014cd67bdb08629202ff718
Mehdi Ait Mouh <mehdi.aitmouh.dev@gmail.com>
2023-02-18 03:18:01 -0800

    Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
-
 /**
- * main - Print string in console using printf func.
- *
+ * main - entry point.
+ * Description: Print string in console using printf func.
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-    	printf("with proper grammar, but the outcome is a piece of art,\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
