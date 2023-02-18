5a3da7b8cfc7578c9918ee4d911f419f203cb1ee
Mehdi Ait Mouh <mehdi.aitmouh.dev@gmail.com>
2023-02-18 03:12:05 -0800

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
-
 /**
- * main - Print string in console using puts func.
- *
+ * main - entry point.
+ * Description: Print string in console using puts func.
  * Return: Always 0 (Success)
  */
 int main(void)
