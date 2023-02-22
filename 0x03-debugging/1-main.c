f36767927163e168b3b47a0fdf06399c9a8c317e
Mehdi Ait Mouh <mehdi.aitmouh.dev@gmail.com>
2023-02-22 06:55:35 -0800

    Like, comment, subscribe

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - problems an infinite loop
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	printf("Infinit loop incoming :(\n");
+
+	i = 0;
+	/*
+	*while (i < 10)
+	*{
+	*	putchar(i);
+	*}
+	*/
+printf("Infit loop avoid! \\o/\n");
+
+return (0);
+}
