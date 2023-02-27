b76b5e872e36eb0f5f5f920722838f385bfa10d1
Mehdi Ait Mouh <mehdi.aitmouh.dev@gmail.com>
2023-02-27 06:29:01 -0800

    returns the length of a string.

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include <string.h>
+/**
+ * _strlen - entry point.
+ * @s: string.
+ * Description: length of a string.
+ * Return: length.
+ */
+int _strlen(char *s)
+{
+	int length;
+
+	for (length = 0; length <= s[length]; length++)
+	{
+		length++;
+	}
+	return (length);
+}
