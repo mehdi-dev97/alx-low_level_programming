106ad16d9ca694797ca8505f39b0ce11f9b789b0
Mehdi Ait Mouh <mehdi.aitmouh.dev@gmail.com>
2023-03-02 12:53:48 -0800

    concatenates two strings.

---
+++ 0x06-pointers_arrays_strings/0-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char s1[98] = "Hello ";
+    char s2[] = "World!\n";
+    char *ptr;
+
+    ptr = _strcat(s1, s2);
+    printf("%s", ptr);
+    return (0);
+}
