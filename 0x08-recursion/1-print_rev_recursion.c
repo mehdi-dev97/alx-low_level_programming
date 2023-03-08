4797525afe20d10854b43a50adc0c7d2883631f1
Mehdi Ait Mouh <mehdi.aitmouh.dev@gmail.com>
2023-03-08 21:44:00 +0100

    prints a string in reverse.

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/*
+ * _print_rev_recursion - prints a string in reverse.
+ * @s: address of string to print
+ * Return: void (Success)
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
