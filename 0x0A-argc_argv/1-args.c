813e56e7d429c09361e353a88b9a922c2b1a5718
Mehdi Ait Mouh <mehdi.aitmouh.dev@gmail.com>
2023-03-10 23:21:54 +0100

    arg count

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints the number of arguments passed into it.
+ * @argc: Number of command line arguments
+ * @argv: Array name
+ * Return: 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	if (argc == 1)
+	{
+		printf("%d\n", argc - 1);
+	}
+	else
+	{
+		for (i = 0; *argv; i++, argv++)
+			;
+
+		printf("%d\n", i - 1);
+	}
+	return (0);
+}
