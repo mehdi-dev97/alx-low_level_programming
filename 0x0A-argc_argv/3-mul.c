d34ddf56c74b733e0a6d2e442a5c7cd89da68636
Mehdi Ait Mouh <mehdi.aitmouh.dev@gmail.com>
2023-03-11 00:04:36 +0100

    multiple number argv and get the result in prompt

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - program that multiplies two numbers.
+ * @argc: Number of command line arguments
+ * @argv: Array name
+ * Return: 0 (Success), 1 (Error)
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+	int total = 0;
+
+	if (argc < 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	for (i = 1; i < argc; i++)
+	{
+		if (i == 1)
+		{
+			total = atoi(argv[i]);
+		}
+		else
+		{
+			total *= atoi(argv[i]);
+		}
+	}
+	printf("%d", total);
+	return (0);
+}
