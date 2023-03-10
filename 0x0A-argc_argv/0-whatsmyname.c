cb29bba7bf759d3ad041a8086a4bdb03679e4be4
Mehdi Ait Mouh <mehdi.aitmouh.dev@gmail.com>
2023-03-10 18:11:59 +0100

    name of the program

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - prints its name, followed by a new line.
+ * Return: 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s", argv[argc * 0]);
+	return (0);
+}
