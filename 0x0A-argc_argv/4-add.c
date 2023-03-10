09593a725abba215ca10fd38a4951ac26ae9fad9
Mehdi Ait Mouh <mehdi.aitmouh.dev@gmail.com>
2023-03-11 00:48:35 +0100

    addition to all argv inserted in prompt

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
+#include <string.h>
 
 /**
  * main - program that adds positive numbers.
@@ -11,32 +12,33 @@
  */
 int main(int argc, char *argv[])
 {
-	int i;
-	int c;
-	int total = 0;
+	int i, j, length, sum;
+	char *ptr;
 
 	if (argc < 2)
-	{
-		printf("0\n");
-		return (0);
-	}
+	printf("0\n");
 	else
 	{
+		sum = 0;
 		for (i = 1; i < argc; i++)
 		{
-			for (c = 0; c <= argv[i][c]; c++)
+			ptr = argv[i];
+			length = strlen(ptr);
+
+			for (j = 0; j < length; j++)
 			{
-				if (isdigit(argv[i][c]) == 0)
+				if (isdigit(*(ptr + j)) == 0)
 				{
 					printf("Error\n");
 					return (1);
 				}
 			}
-	
-			total += atoi(argv[i]);
 
+			sum += atoi(argv[i]);
 		}
-		printf("%d\n", total);
+
+		printf("%d\n", sum);
 	}
 	return (0);
 }
+
