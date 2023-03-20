dbbec3d0eeb699deea88f2236fbd6cc1c8cca05c
Mehdi Ait Mouh <mehdi.aitmouh.dev@gmail.com>
2023-03-20 23:00:45 +0000

    Define a new type struct dog with his elements

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,17 @@
 #ifndef DOG_H
 #define DOG_H
 
-typedef struct dog dog;
+/*
+ * File: dog.h
+ * Desc: Header file that defines a new type struct dog.
+ */
+
+/**
+ * struct dog - A new type describing a dog.
+ * @name: The name of the dog.
+ * @age: The age of the dog.
+ * @owner: The owner of the dog.
+ */
 struct dog {
 	char *name;
 	float age;
