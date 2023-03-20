c8c01de990312e5d5a27afeb44f8a23d22f8ac1e
Mehdi Ait Mouh <mehdi.aitmouh.dev@gmail.com>
2023-03-20 18:02:35 +0000

    macro SUM that computes the sum of the numbers x and y

--- 0x0D-preprocessor/4-sum.h
+++ 0x0D-preprocessor/4-sum.h
@@ -3,8 +3,8 @@
 
 /*
  * File: 4-sum.h
- * Desc: Header function-like macro SUM(x, y)
- * 	 that computes the sum of the numbers x and y.
+ * Desc: Header file that defines a function-like macro SUM(x, y)
+ *       that computes the sum of the numbers x and y.
  */
 
 #define SUM(x, y) ((x) + (y))
