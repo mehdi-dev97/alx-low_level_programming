cd9a34887d511b6919c15ebdf1fc2fe58d59d98e
Mehdi Ait Mouh <mehdi.aitmouh.dev@gmail.com>
2023-03-24 03:57:06 -0700

    Fix comment block description on calc func

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,13 +1,10 @@
 #include "3-calc.h"
 
 /**
- *  * get_op_func - function pointer that selects the correct function to perform
- *   * the operation asked by the user
- *    * @s: the operator given by the user
- *     *
- *      * Return: pointer to the function that corresponds to the
- *       * operator given as a parameter
- *        */
+ * get_op_func - function pointer that selects the correct function to perform.
+ * @s: the operator given by the user
+ * Return: result of calculation
+ */
 int (*get_op_func(char *s))(int, int)
 {
 	op_t ops[] = {
