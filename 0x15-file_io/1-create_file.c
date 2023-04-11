c194cbe020004b034742b639f84aa33def6db8d2
Mehdi Ait Mouh <mehdi.aitmouh.dev@gmail.com>
2023-04-10 17:03:23 -0700

    function that appends text at the end of a file.

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,7 +20,10 @@ int create_file(const char *filename, char *text_content)
 		len++;
 	}
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-	wr = write(fd, text_content, len);
+	if(len > 0)
+	{
+		wr = write(fd, text_content, len);
+	}
 	if (fd == -1 || wr == -1)
 	{
 		return (-1);
