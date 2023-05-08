37ee574a2ab174078cbeaa5c46c62c3bc7ac4fd0
Mehdi Ait Mouh <67864826+mehdi-dev97@users.noreply.github.com>
2023-05-08 16:41:50 +0100

    Fix create file bug

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,33 +1,24 @@
 #include "main.h"
-
 /**
- * create_file - create a file
- * @filename: file name
- * @text_content: text content of file
- * Return: 1 (Success), -1 On error
+ * create_file - creates a file
+ * @filename: the filename.
+ * @text_content: the content writed in the file.
+ * Return: 1 on success. -1 on failure
  */
 int create_file(const char *filename, char *text_content)
 {
-	ssize_t fd, wr;
-	int len = 0;
-
+	int nop, nwr, len = 0;
 	if (filename == NULL)
-	{
 		return (-1);
-	}
-	while (text_content[len] != '\0')
+	if (text_content != NULL)
 	{
-		len++;
+		for (len = 0; text_content[len];)
+			len++;
 	}
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-	if(len > 0)
-	{
-		wr = write(fd, text_content, len);
-	}
-	if (fd == -1 || wr == -1)
-	{
+	nop = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	nwr = write(nop, text_content, len);
+	if (nop == -1 || nwr == -1)
 		return (-1);
-	}
-	close(fd);
+	close(nop);
 	return (1);
 }
